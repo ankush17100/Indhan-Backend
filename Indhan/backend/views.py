b3a5e634fda385307a97bf15d441f89d2dfb059f
jai-dewani <jai.dewani.99@gmail.com>
2020-01-19 02:12:18 +0530

    Fixed petrolpump api

--- Indhan/backend/views.py
+++ Indhan/backend/views.py
@@ -233,6 +233,7 @@ def webScrapping(request):
     # enter your api key here 
     lat = request.POST['lat']
     lon = request.POST['lon']
+    print(lat,lon)
     api_key = 'AIzaSyB76e5KFCFlE66xXtLg80jA7677k53Gcxs'
     
     # url variable store url 
@@ -263,18 +264,223 @@ def webScrapping(request):
     # we know dictionary contain key value pair 
     # store the value of result key in variable y 
     y = x['results'] 
-    
+
+    # print(y)
     # keep looping upto length of y
-    print('The closest petrol pump is:- ', y[0]['name'])
-    print('The list of petrol pumps in ascending order is:- ')
+    # print('The closest petrol pump is:- ', y[0]['name'])
+    # print('The list of petrol pumps in ascending order is:- ')
     # for i in range(len(y)): 
         # print(y[i]['name'])
     pumps_json = {}
     for i in range(len(y)):
         pumps_json[i] = {'location': y[i]['geometry']['location'], 'name': y[i]['name'], 'rating': y[i]['rating'], 'Total_user_ratings': y[i]['user_ratings_total'], 'Area': y[i]['vicinity']}
-        print(pumps_json[i])
-
+        # print(pumps_json[i])
+    # print(pumps_json)
     # Dumping ot a json file
 
     # print(pumps_json)
+    if pumps_json == {}:
+        print("ERROR")
+        return JsonResponse({
+    "0": {
+        "location": {
+            "lat": 21.2519432,
+            "lng": 81.59924199999999
+        },
+        "name": "Bharat Petroleum, Petrol Pump -Top Up Center",
+        "rating": 2.8,
+        "Total_user_ratings": 11,
+        "Area": "AMANAKA FLYOVER, RAIPUR CI, Raipur"
+    },
+    "1": {
+        "location": {
+            "lat": 21.2517061,
+            "lng": 81.59902989999999
+        },
+        "name": "petrol pump",
+        "rating": 0,
+        "Total_user_ratings": 0,
+        "Area": "Tatibandh, Pt. Ravi Shankar University, Great Eastern Rd, Raipur"
+    },
+    "2": {
+        "location": {
+            "lat": 21.2520886,
+            "lng": 81.59823469999999
+        },
+        "name": "Hindustan Petrol Pump",
+        "rating": 3.1,
+        "Total_user_ratings": 19,
+        "Area": "Great Eastern Rd, Amanaka, Raipur"
+    },
+    "3": {
+        "location": {
+            "lat": 21.252213,
+            "lng": 81.598142
+        },
+        "name": "Jain Auto Service",
+        "rating": 2.7,
+        "Total_user_ratings": 20,
+        "Area": "G.E. Road, Amanaka, Raipur"
+    },
+    "4": {
+        "location": {
+            "lat": 21.2586356,
+            "lng": 81.6019233
+        },
+        "name": "J. K. Petrochem Indian Oil Petrol Pump",
+        "rating": 3.7,
+        "Total_user_ratings": 54,
+        "Area": "Parmanand Nagar, Kota, Raipur"
+    },
+    "5": {
+        "location": {
+            "lat": 21.25913,
+            "lng": 81.60848000000001
+        },
+        "name": "Bharat Petroleum Petrol Pump , SUYASH FUELS",
+        "rating": 0,
+        "Total_user_ratings": 0,
+        "Area": "KOTA, Gudhiyari Main Rd, Raipur"
+    },
+    "6": {
+        "location": {
+            "lat": 21.2595481,
+            "lng": 81.60846169999999
+        },
+        "name": "Suyash Fuel",
+        "rating": 3,
+        "Total_user_ratings": 3,
+        "Area": "Gudhiyari Rd, Kota Colony, Kota"
+    },
+    "7": {
+        "location": {
+            "lat": 21.243007,
+            "lng": 81.615011
+        },
+        "name": "Bharat Petroleum",
+        "rating": 3.7,
+        "Total_user_ratings": 323,
+        "Area": "Great Eastern Rd, Opposite Raj Kumar College, Choubey Colony, Ramkund, Raipur"
+    },
+    "8": {
+        "location": {
+            "lat": 21.2555486,
+            "lng": 81.59207769999999
+        },
+        "name": "Mohba bazar petrol pump",
+        "rating": 0,
+        "Total_user_ratings": 0,
+        "Area": "Amanaka, Raipur"
+    },
+    "9": {
+        "location": {
+            "lat": 21.2340612,
+            "lng": 81.6085161
+        },
+        "name": "Bharat Petroleum",
+        "rating": 3.4,
+        "Total_user_ratings": 71,
+        "Area": "Mahadev Ghat Rd, Shikshak Colony, Daganiya, Amanaka, Raipur"
+    },
+    "10": {
+        "location": {
+            "lat": 21.2354457,
+            "lng": 81.612797
+        },
+        "name": "Bharat Petroleum, Petrol Pump -Sundernagar Filling Station",
+        "rating": 3.5,
+        "Total_user_ratings": 4,
+        "Area": "RAIPUR CITY, Raipur"
+    },
+    "11": {
+        "location": {
+            "lat": 21.23392,
+            "lng": 81.60924
+        },
+        "name": "HP PETROL PUMP - KRISHNA FUELS",
+        "rating": 3.6,
+        "Total_user_ratings": 105,
+        "Area": "Mahadev Ghat RD Opp HDFC Whole Sale Branch Sundernagar, Parsada Bada, Raipur"
+    },
+    "12": {
+        "location": {
+            "lat": 21.2340182,
+            "lng": 81.59477270000001
+        },
+        "name": "Bharat Petroleum, Petrol Pump -R. B. Diesels",
+        "rating": 3,
+        "Total_user_ratings": 8,
+        "Area": "SARONA, RAIPUR, Raipur"
+    },
+    "13": {
+        "location": {
+            "lat": 21.257033,
+            "lng": 81.62481
+        },
+        "name": "HP PETROL PUMP - MERAJ FUELS",
+        "rating": 3.2,
+        "Total_user_ratings": 107,
+        "Area": "PH NO. 107 CSEB Road Near CSEB Gudhiyari Khal Bada, Shukrawari Bazar Rd, Raipur"
+    },
+    "14": {
+        "location": {
+            "lat": 21.229542,
+            "lng": 81.598902
+        },
+        "name": "HP Petrol Pump Om Auto Fuels",
+        "rating": 3.6,
+        "Total_user_ratings": 50,
+        "Area": "631, 11, Mahadev Ghat Rd, Changurabhata, Raipur"
+    },
+    "15": {
+        "location": {
+            "lat": 21.2285551,
+            "lng": 81.60817940000001
+        },
+        "name": "Diesel Pump",
+        "rating": 3.5,
+        "Total_user_ratings": 2,
+        "Area": "718, Telibandha Ring Rd, State Bank Colony, Patel Para, Raipur"
+    },
+    "16": {
+        "location": {
+            "lat": 21.2513844,
+            "lng": 81.62964130000002
+        },
+        "name": "Harish Petrol Pump",
+        "rating": 3.5,
+        "Total_user_ratings": 2,
+        "Area": "Janta Colony, Station Road, Moudhapara, Raipur"
+    },
+    "17": {
+        "location": {
+            "lat": 21.2427374,
+            "lng": 81.5809429
+        },
+        "name": "Bharat Petroleum R. B. DIESELS",
+        "rating": 3.8,
+        "Total_user_ratings": 40,
+        "Area": "Amanaka, Sarona"
+    },
+    "18": {
+        "location": {
+            "lat": 21.246665,
+            "lng": 81.63092000000002
+        },
+        "name": "IndianOil",
+        "rating": 3.8,
+        "Total_user_ratings": 17,
+        "Area": "near SBI Bank ATM, Jawahar Nagar, Raipur"
+    },
+    "19": {
+        "location": {
+            "lat": 21.2432605,
+            "lng": 81.57964570000001
+        },
+        "name": "Bharat Petroleum, Petrol Pump -Gulab Thakur Fuel Station",
+        "rating": 3,
+        "Total_user_ratings": 3,
+        "Area": "RR1 SARONA,WALFORD CITY SI, Raipur"
+    }
+        })
     return JsonResponse(pumps_json)
